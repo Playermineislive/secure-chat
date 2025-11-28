553a30d1ae15cce37a6fd3d5a02ee285e2c6a18b
Playermineislive <162860904+Playermineislive@users.noreply.github.com>
2025-11-28 22:30:45 +0530

    Delete server/server.tso

--- server/server.ts
+++
@@ -1,8 +0,0 @@
-import { createAppServer } from "./src/index";
-
-const port = process.env.PORT || 3000;
-const server = createAppServer();
-
-server.listen(port, () => {
-  console.log("Server running on port " + port);
-});
