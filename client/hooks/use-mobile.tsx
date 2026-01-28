e041e91324e63449884f0eb4d4ce7e8fdffde8a3
Playermineislive <162860904+Playermineislive@users.noreply.github.com>
2026-01-28 11:40:13 +0530

    Refactor useIsMobile hook for improved state handling

--- client/hooks/use-mobile.tsx
+++ client/hooks/use-mobile.tsx
@@ -1,21 +1,26 @@
-import * as React from "react";
+import * as React from "react"
 
-const MOBILE_BREAKPOINT = 768;
+const MOBILE_BREAKPOINT = 768
 
 export function useIsMobile() {
-  const [isMobile, setIsMobile] = React.useState<boolean | undefined>(
-    undefined,
-  );
+  const [isMobile, setIsMobile] = React.useState(false)
 
   React.useEffect(() => {
-    const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`);
+    const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`)
+    
     const onChange = () => {
-      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
-    };
-    mql.addEventListener("change", onChange);
-    setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
-    return () => mql.removeEventListener("change", onChange);
-  }, []);
+      setIsMobile(mql.matches)
+    }
 
-  return !!isMobile;
+    // Set initial value
+    onChange()
+
+    // Add listener
+    mql.addEventListener("change", onChange)
+    
+    // Cleanup
+    return () => mql.removeEventListener("change", onChange)
+  }, [])
+
+  return isMobile
 }
